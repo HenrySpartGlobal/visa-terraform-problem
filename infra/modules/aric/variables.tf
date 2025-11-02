9dfeb12353d667394ceab0020b72e0462902952f
Henry Koleoso <henry_koleoso@hotmail.co.uk>
2025-11-02 15:58:20 +0000

    update description for env

--- infra/modules/aric/variables.tf
+++ infra/modules/aric/variables.tf
@@ -3,6 +3,7 @@ variable "project" {
 }
 
 variable "env" {
+  description = "Environment name and it's tier. For example 'prod', 'preprod', 'test' etc"
   type = object({
     name = string
     tier = string
